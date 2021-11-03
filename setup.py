463e92fa44088121a04697e3e3308d320b0831ce
talnoznisky <tal.noznisky@gmail.com>
2021-11-02 22:18:22 -0500

    updated setup info

--- setup.py
+++ setup.py
@@ -8,7 +8,13 @@ setup(
     name = 'homage-to-the-square',
     packages=['homage'],
     description = 'runs an homage to the square a la josef albers',
-    version = '0.0.1',
+    version = '1.0.1',
+    author="Tal Noznisky",
+    keyword="josef albers, bauhaus, homage to the square",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/talnoznisky/homage-to-the-square',
     python_requires='>=3.0', # any python greater than 2.7
     entry_points='''
             [console_scripts]
